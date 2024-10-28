1c04bc29d895870f96d4094f2d0bfeebf477ad4e
marianavillarroel1 <165936075+marianavillarroel1@users.noreply.github.com>
2024-10-28 19:00:09 -0400

    Clase Facturas v1.2 (SIN PROBAR)

--- Facturas.cs
+++ Facturas.cs
@@ -1,11 +1,12 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace pruebas_Facturacion.Models
 {
