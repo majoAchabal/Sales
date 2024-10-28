dbe984fd83fd2d343f08e66f2c1cc474cb5111bf
Maria Jose <165935708+majoAchabal@users.noreply.github.com>
2024-10-28 00:53:50 -0400

    Add files via upload

---
+++ Clientes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo
+{
+    public partial class Clientes : Form
+    {
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Menu nuevaVentana = new Menu();
+            nuevaVentana.ShowDialog();
+            this.Hide();
+        }
+    }
+}
