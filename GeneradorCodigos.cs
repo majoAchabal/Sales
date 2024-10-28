5eb3d0bcea33e85a1c59b7712f2ad9f53049059e
marianavillarroel1 <165936075+marianavillarroel1@users.noreply.github.com>
2024-10-28 18:24:30 -0400

    Clase GeneradorCodigos (SIN PROBAR)

---
+++ GeneradorCodigos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas_Facturacion.Models
+{
+    internal class GeneradorCodigos
+    {
+        private static readonly Random random = new Random();
+
+        public static string GenerarCUIS()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 16)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GenerarCUFD()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_$#@";
+            return new string(Enumerable.Repeat(chars, 64)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
