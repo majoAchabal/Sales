0fdcf0f98b80abcc1fe4ce6a76da16e1c9151fdc
Maria Jose <165935708+majoAchabal@users.noreply.github.com>
2024-10-29 04:22:40 -0400

    Add files via upload

---
+++ GenerarCodigos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototipo
+{
+    public class GenerarCodigos
+    {
+        private static readonly Random random = new Random();
+
+        public static string GenerarCUIS()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 16)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GenerarCUFD()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_$#@";
+            return new string(Enumerable.Repeat(chars, 64)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
